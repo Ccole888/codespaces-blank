e37643690ab3e4a7c759793d32e46bc557136a42
Colin Cole <110618749+Ccole888@users.noreply.github.com>
2026-02-13 15:12:43 +0000

    Task2

---
+++ taskTwo/task2.s
@@ -0,0 +1,20 @@
+    .section .bss
+    .lcomm ram, 256          
+    .global ram
+
+    .section .text
+    .global fill_Ram_Indirect
+
+fill_Ram_Indirect:
+    lea ram(%rip), %rdi      
+    add $0x50, %rdi          
+    mov $9, %rcx             
+    mov $0xFF, %al           
+
+loop_fill:
+    movb %al, (%rdi)         
+    inc %rdi
+    loop loop_fill
+    ret
+
+.section .note.GNU-stack,"",@progbits
